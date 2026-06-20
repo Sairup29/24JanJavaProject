5e2b90a94773b931c7411c28a5064acb5be7619b
“niharikaaitham” <niharika.aitham@gmail.com>
2026-06-20 02:37:32 +0100

    VEP-001||Modified AbstractClass

---
+++ src/AbstractClass/NewTestRef.java
@@ -0,0 +1,17 @@
+package AbstractClass;
+
+public class NewTestRef {
+
+      public void test() {
+              System.out.println("complete method");
+          }
+    public void test1() {
+    }
+
+    public  void test2(){
+
+    }
+}
+
+
+
